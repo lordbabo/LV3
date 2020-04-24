52d8ce1b46fce0aeb12cd3f66ffa313a62fdf627
lordbabo <slaven55555@gmail.com>
2020-04-24 14:35:03 +0200

    Add project files.

---
+++ LV3/MatrixGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV3
+{
+    class MatrixGenerator
+    {
+        public static MatrixGenerator instance;
+        private Random random;
+
+        private MatrixGenerator()
+        {
+            random = new Random();
+        }
+
+        public static MatrixGenerator GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new MatrixGenerator();
+            }
+            return instance;
+        }
+
+        public float[][] CreateMatrix(int height, int width)
+        {
+            float[][] matrix = new float[height][];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = new float[width];
+            }
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    matrix[i][j] = (float)random.NextDouble();
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ LV3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LV3
+{
+    class Program
+    {
+        // Deep copy nije potreban u ovom primjeru jer Dataset nema slozene tipove podatka.
+
+        static void Main(string[] args)
+        {
+            //zadatak 1
+            //Console.WriteLine("Originalni csv: ");
+            //Prototype file1 = new Dataset(@"C:\Users\slave\Desktop\zadatak1.csv");
+            //FilePrint((Dataset)file1);
+            //Console.WriteLine("Deep Copy csv: ");
+            //Prototype file2 = file1.Clone();
+            //FilePrint((Dataset)file2);
+
+            //zadatak 2
+            //MatrixGenerator m = MatrixGenerator.GetInstance();
+            //float[][] matrix = m.CreateMatrix(3, 3);
+            //FilePrint(matrix);
+
+        }
+        static void FilePrint(Dataset d)
+        {
+            foreach (List<string> listString in d.GetData())
+            {
+                foreach (string s in listString)
+                {
+                    Console.Write(s);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        static void FilePrint(float[][] f)
+        {
+            for (int i = 0; i < f.Length; i++)
+            {
+                for (int j = 0; j < f[i].Length; j++)
+                {
+                    Console.Write(f[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
